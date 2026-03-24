c2a1b8df20c16b4aae34dcd705b8310cd335338f
Thomas van Steen <thomas@webthomas.com>
2026-03-24 10:19:53 +0200

    started adding the product rows

--- js/products.js
+++ js/products.js
@@ -1,3 +1,23 @@
-function addProduct(evt) {
-    var element = "";
+let rowId = 0;
+
+function addProduct(name = '', model = '', quantity = '', price='') {
+    const id = rowId++;
+    const row = document.createElement("div");
+
+    row.id = `row-${id}`;
+    row.className = "product-row"
+
+    row.innerHTML = `
+    <input style="flex: 6;" style="min-width: 0;" placeholder="Name" value="${name}" />
+    <input style="flex: 4;" style="min-width: 0;" placeholder="Model" value="${model}" />
+    <input style="flex: 4;" style="min-width: 0;" placeholder="Quanity" value="${quantity}" />
+    <input style="flex: 4;" style="min-width: 0;" placeholder="Price" value="${price}" />
+    <button onclick="removeRow(${id})" class="remove-button" style="flex: 1;"><img src="icons/trash.svg" alt="Delete" width="20" height="20"></button>
+    `
+
+    document.getElementById("rows").appendChild(row);
+}
+
+function removeRow(id) {
+    document.getElementById(`row-${id}`)?.remove();
 }
